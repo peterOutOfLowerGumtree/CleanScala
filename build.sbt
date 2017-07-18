4af9c22247ae6c1a29dcd19348315f45f585455e
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-07-18 13:24:02 +0100

    code v1, begin testing

--- build.sbt
+++ build.sbt
@@ -1,6 +1,13 @@
+
 name := "CleanScala"
 
 version := "1.0"
 
 scalaVersion := "2.12.1"
-        
+
+wartremoverErrors ++= Warts.unsafe
+
+coverageEnabled := true
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
---
+++ src/main/scala/main.scala
@@ -0,0 +1,33 @@
+
+object main {
+
+  def main(args: Array[String]): Unit = {
+    longAndShort("000000000176867654456541")
+  }
+
+  def longAndShort(input: String): Unit = {
+    try {
+      val numberTest = input.toLong
+      println(langOutput(input, english))
+      println(langOutput(input, french))
+    } catch {
+      case e: Exception => println("Enter a valid number")
+    }
+  }
+
+  lazy val english: List[String] = List(" ", " thousand and ", " million ", " billion ", " trillion ", " quadrillion ", " quintillion ", " sextillion ")
+  lazy val french: List[String] = List(" ", " mille et ", " million ", " milliard ", " billion ", " billiard ", " trillion ", " triliard ")
+
+  def langOutput(input: String, lang: List[String]): String = {
+    val in = input.replaceFirst("^0*", "").reverse.grouped(3).toList.map(_.reverse.toList.mkString)
+    val out = for (i <- in.indices) yield {
+      i match {
+        case _ if i > lang.length => in(i) + " "
+        case _ if in(i) != "000" => in(i) + lang(i)
+        case 0 if in(0) != "000" && in(1) == "000" => " and " + in(i)
+        case _ => ""
+      }
+    }
+    out.reverse.mkString
+  }
+}
